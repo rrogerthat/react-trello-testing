4c4d6a14d6e8a19bb857c32ec21ae7c481da0b1e
Roger Hwang <hroger19@gmail.com>
2018-06-26 11:23:43 -0700

    added .travis.yml file to integrate Travis CI

--- src/components/add-form.test.js
+++ src/components/add-form.test.js
@@ -10,28 +10,28 @@ describe('<AddForm />', () => {
 
     it('Renders the add button initially', () => {
         const wrapper = shallow(<AddForm />);
-        expect(wrapper.hasClass('add-button')).toEqual(true);
+        expect(wrapper.hasClass('add-button')).toEqual(true);   //add button is added by checking className
     });
 
-    it('Should render the add form when editing', () => {
+    it('Should render the add form when editing', () => {   //form displays thru boolean value
         const wrapper = shallow(<AddForm />);
-        wrapper.instance().setEditing(true);
-        wrapper.update();
+        wrapper.instance().setEditing(true);    //make an instance of component AddForm to apply method to
+        wrapper.update();                       //since Enzyme won't auto. rerender component
         expect(wrapper.hasClass('add-form')).toEqual(true);
     });
 
-    it('Should switch to editing when the add button is clicked', () => {
-        const wrapper = shallow(<AddForm />);
-        wrapper.simulate('click');
-        expect(wrapper.state('editing')).toEqual(true);
+    it('Should switch to editing when the add button is clicked', () => {   //check thru user interaction (click)
+        const wrapper = shallow(<AddForm />);       //no update() needed for simulate method
+        wrapper.simulate('click');      //simulates DOM events to ensure component responds correctly to user interaction
+        expect(wrapper.state('editing')).toEqual(true); //when add button clicked, editing property in state is now true
     });
 
     it('Should fire the onAdd callback when the form is submitted', () => {
         const callback = jest.fn();
         const wrapper = mount(<AddForm onAdd={callback} />);
         const value = 'Foobar';
-        wrapper.instance().setEditing(true);
-        wrapper.update();
+        wrapper.instance().setEditing(true);    
+        wrapper.update();   //to render form
         wrapper.find('input[type="text"]').instance().value = value;
         wrapper.simulate('submit');
         expect(callback).toHaveBeenCalledWith(value);
